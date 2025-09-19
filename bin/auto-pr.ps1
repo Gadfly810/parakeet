3a1cfe1c88a3abeee7e3841260cade2e41dc0210
Gadfly810 <138390711+Gadfly810@users.noreply.github.com>
2025-09-19 23:40:46 +0800

    Update default upstream parameter in auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "gadfly/parakeet:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
